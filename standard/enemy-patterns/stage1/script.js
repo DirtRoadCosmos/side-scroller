05b1235b2688a23ae7fb58eb1a2f3bd7f6590c8c
andytilia <andytilia@users.noreply.github.com>
2023-04-28 12:17:11 -0400

    Create script.js

---
+++ standard/enemy-patterns/stage1/script.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 300;
+const CANVAS_HEIGHT = canvas.height = 500;
+
+const enemy1 = {
+  x: 10,
+  y: 50,
+  width: 100,
+  height: 100,
+}
+
+function animate(){
+  ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+}
+animate();
